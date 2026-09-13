11cc22bfa72fb9c9c36fe370549a1d6e7709934a
Pizels29 <aarnavdave29@gmail.com>
2026-09-12 22:02:31 -0400

    Built skeleton and organized files. Will add some more code after meeting tomorrow

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/lancersBotConfig.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.config;
+
+public final class
+lancersBotConfig {
+
+    //Motor configurations from the hardware map
+    public static final String FRONT_LEFT_MOTOR = "frontLeftMotor";
+    public static final String REAR_LEFT_MOTOR = "rearLeftMotor";
+    public static final String FRONT_RIGHT_MOTOR = "frontRightMotor";
+    public static final String REAR_RIGHT_MOTOR = "rearRightMotor";
+
+    public final static String INTAKE_MOTOR = "intakeMotor"; // EH motor port 0
+    public final static String OUTTAKE_MOTOR = "outtakeMotor"; // EH motor port 1
+    public final static String OUTTAKE_MOTOR_TWO = "outtakeMotorTwo"; // EH motor port 2
+    public final static String IMU = "imu_ex"; // Expansion Hub IMU
+
+   /* public final static String OUTTAKE_ROTATION_MOTOR = "outtakeRotationMotor";
+
+    public final static String OUTTAKE_SERVO = "outtakeServo"; // EH servo port 0
+
+    public final static String LIMELIGHT = "limelight";
+
+    public final static String PINPOINT = "Buzzy x Meahul 6767";
+
+    Legacy from last year
+    */
+}
